83879db116745ce09dd87b5de9bf575f0c33c591
Mikko Maja <mikmaj88@gmail.com>
2018-10-10 17:17:48 +0300

    Add movement up and down

--- ship.py
+++ ship.py
@@ -15,25 +15,34 @@ class Ship():
 
         # Start each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
         self.rect.bottom = self.screen_rect.bottom
 
         # Store a decimal value for the ship's center
-        self.center = float(self.rect.centerx)
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
 
         # Moving flags
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on the movement flag"""
         # Update the ship's center value, not the rect
-        if self.moving_right:
-            self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
-            self.center -= self.ai_settings.ship_speed_factor
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > 0:
+            self.centery -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
 
         # Update rect object from self.center
-        self.rect.centerx = self.center
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
 
     def blitme(self):
         """Draw the ship at its current location"""
