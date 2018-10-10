723aed7ddfa4fcc84bfe631ce727d901372b0822
Mikko Maja <mikmaj88@gmail.com>
2018-10-10 16:11:41 +0300

    Add game_functions.py

--- alien-invasion.py
+++ alien-invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -19,18 +20,8 @@ def run_game():
 
     # Start the main loop
     while True:
-
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
